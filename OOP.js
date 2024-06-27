3212baf275be8f62f8ace1e938dc971fc09cfeb1
Kilian Tavares <117239789+Darkskindanger@users.noreply.github.com>
2024-06-27 20:48:32 +1000

    Update OOP.js

--- OOP.js
+++ OOP.js
@@ -1,106 +1,104 @@
-//1.
- class Person{
-    // 'Setter'
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-
-    };
-
-    // Good practice for security is to set a 'getter' for each property of a constructor
-    getName=()=>{return this.name;};
-
-    getAge=()=>{return this.age};
-
-
+//
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.job = '';
+  }
+  getName = () => {
+    return this.name;
+  };
+  getAge = () => {
+    return this.age;
+  };
+  getJob = (job) => {
+    return this.job;
+  };
 }
 
-let person1 = new Person('pedro',19)
-let person2 = new Person('James',22)
-console.log(person1.getName())
-console.log(person2.getName())
-console.log(person1.getAge())
-console.log(person2.getAge())
+let Daniel = new Person('Daniel', 28);
 
-//2. 
-class House{
-    constructor(address,price,residents){
-        this.address=address;
-        this.price=price;
-        this.residents=residents;
-    }
-    getAddress = ()=>{return this.address}
-    getPrice=()=>{return this.price}
-    getResidents=()=>{return this.residents}
-
-    addResident = (resident)=>{
-    this.residents.push(resident)
-    }
+class Home {
+  constructor(address, price, residents) {
+    this.address = address;
+    this.price = price;
+    this.residents = residents;
+  }
+  getAddress = () => {
+    return this.address;
+  };
+  getPrice = () => {
+    return this.price;
+  };
+  getResidents = () => {
+    return this.residents;
+  };
 }
 
-let Pedro = new Person('Pedro',19)
-let David = new Person('David',22)
+let home1 = new Home('1 default lane', 2900000, [Daniel]);
+console.log(home1);
 
-let house = new House('1 random lane', 290000,[Pedro,David])
-console.log(house.getResidents())
+// Abstraction: Only showing necessary details needed to whomever is using the object
 
-let Paulo = new Person('Paulo',24)
-house.addResident(new Person('Peter',24))
-console.log(house.getResidents())
-
-// Practicing abstraction: Adding to newly added 'job' field
-
-class Person2{
-
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-        this.job="" //expecting a string
-    };
-
-    getName=()=>{return this.name;};
-
-    getAge=()=>{return this.age};
-
-    setJob=(job)=>{this.job = job};
+class Person2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.job = '';
+  }
+  getName = () => {
+    return this.name;
+  };
+  getAge = () => {
+    return this.age;
+  };
+  setJob = (job) => {
+    this.job = job;
+  };
 }
 
-let Jim = new Person2('Jim',40)
-let house2 = new House('2 Random Ave', 200000,[])
-// house2.addResident(new Person2('Nick',20))
-house2.addResident(Jim)
-Jim.setJob('Developer')
-console.log(house2.getResidents())
+let Chris = new Person2('Chris', 30);
+Chris.setJob('Developer');
+console.log(Chris);
 
-// 
-// class Shopper{
-//     constructor(name,age,bankBalance){
-//         this.name=name;
-//         this.age=age;
-//         this.bankBalance=bankBalance;
-//     }
-//     getName = ()=>{return this.name}
-//     getAge = ()=>{return this.age}
-//     getBalance = ()=>{return this.bankBalance}
-// }
+// Encapsulation - Privatisation of code
 
-// let Dave = new Shopper('Dave',28,2000)
-// let Mike = new Shopper('Mike',18,4000)
+class Person3 {
+  constructor(name, age) {
+    this._name = name; //Adding an underscore before the variable name tells other developers not to access/change properties individually
+    this._age = age;
+    this._job = '';
+  }
+  getName = () => {
+    return this.name;
+  };
+  getAge = () => {
+    return this.age;
+  };
+  getJob = (job) => {
+    return this.job;
+  };
+}
 
-// class ShoppingCart{
-//     constructor(item,price,qty,customers){
-//         this.item=item;
-//         this.price=price;
-//         this.qty=qty;
-//         this.customers=customers
-//     }
+//Inheritance:
 
-//     getItem = ()=>{return this.item};
-//     getPrice = ()=>{return this.price};
-//     getQty = ()=>{return this.qty};
-//     getCustomers = ()=>{return this.customers}
-// }
+class Programmer extends Person {
+  // 'extends' is as stated, 1 child class extends a parent class (giving access to everything inside the parent class)
+  constructor(name, age, company, salary, language) {
+    super(name, age); // 'super() is used to create an instance of the parent class (Person) and specifies the intended classes to be filled
+    this.company = company;
+    this.salary = salary;
+    this.language = language;
+  }
 
-// let cart1 = new ShoppingCart('Chicken Breast',99,2,[Dave,Mike])
-// console.log(cart1)
+  sayHi = () => {
+    console.log(
+      `Hello, i am a programmer! My name is ${this.getName()} and i work for ${
+        this.company
+      }`
+    );
+  };
+}
 
+let programmer = new Programmer('Pedro', 19, 'Twitch', 100000, 'Javascript');
+programmer.sayHi();
